95a568606092dac3e84180a202c463ef640b9f0e
Shannen Saikia <shannen.saikia@gmail.com>
2025-11-03 00:22:22 +0530

    wrapping theme provider with app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Web3Provider } from './Web3Context';
+import { ThemeProvider } from './ThemeContext';
 import Home from './Home';
 import Identity from './pages/Identity';
 import Certificates from './pages/Certificates';
@@ -10,18 +11,20 @@ import Admin from './pages/Admin';
 
 function App() {
   return (
-    <Web3Provider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/identity" element={<Identity />} />
-          <Route path="/certificates" element={<Certificates />} />
-          <Route path="/voting" element={<Voting />} />
-          <Route path="/services" element={<ServiceRequests />} />
+    <ThemeProvider>
+      <Web3Provider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/identity" element={<Identity />} />
+            <Route path="/certificates" element={<Certificates />} />
+            <Route path="/voting" element={<Voting />} />
+            <Route path="/services" element={<ServiceRequests />} />
           <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
-    </Web3Provider>
+      </Web3Provider>
+    </ThemeProvider>
   );
 }
 
